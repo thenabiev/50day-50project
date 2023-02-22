93426f3ad92da8bc8f27984967608805ec6afe08
thenabiev <sahinnabee@gmail.com>
2023-02-22 23:16:41 +0400

    .

---
+++ 13day-btn-ripple/btn.js
@@ -0,0 +1,14 @@
+const btn=document.querySelectorAll('a');
+btn.forEach((button)=>{
+    button.onclick=function(e){
+        let x=e.clientX-e.target.offsetLeft;
+        let y=e.clientY-e.target.offsetTop;
+        let ripple=document.createElement('span')
+        ripple.style.left=`${x}px`;
+        ripple.style.top=`${y}px`;
+        this.appendChild(ripple);
+        setTimeout(function(){
+            ripple.remove()
+        },600)
+    }
+})
