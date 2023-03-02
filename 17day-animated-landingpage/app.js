9923b7f6775e8b02c815de54725a9327676aa870
thenabiev <sahinnabee@gmail.com>
2023-03-02 11:29:02 +0400

    .

--- 17day-animated-landingpage/app.js
+++ 17day-animated-landingpage/app.js
@@ -29,3 +29,63 @@ root.style.setProperty('--marque-elements', marqueContent.children.length);
 for(let i=0; i<marqueElementsDisplayed; i++){
     marqueContent.appendChild(marqueContent.children[i].cloneNode(true))
 }
+
+const marqueAni=document.getElementById('marque-ani');
+const marqueFixed=document.getElementById('marque-fixed');
+
+function screenChange(e){
+    if(e.matches){
+        marqueAni.classList.add('hide');
+        marqueFixed.classList.remove('hide');
+    }else{
+        marqueFixed.classList.add('hide');
+        marqueAni.classList.remove('hide');
+    }
+}
+
+const mediaQuery=window.matchMedia('(min-width:800px)');
+
+mediaQuery.addListener(screenChange);
+
+screenChange(mediaQuery);
+
+// ANIMATIONS
+const observer=new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add('show')
+        }else{
+            entry.target.classList.remove('show')
+        }
+    })
+});
+
+const hiddenElements=document.querySelectorAll('.hidden');
+
+hiddenElements.forEach((el)=>{
+    observer.observe(el)
+})
+
+const hiddenBlur=document.querySelectorAll('.hiddenblr');
+
+hiddenBlur.forEach((el)=>{
+    observer.observe(el)
+})
+
+const slideRight=document.querySelectorAll('.slide-right');
+
+slideRight.forEach((el)=>{
+    observer.observe(el)
+})
+
+const slideLeft=document.querySelectorAll('.slide-left');
+
+slideLeft.forEach((el)=>{
+    observer.observe(el)
+})
+
+const enlarge=document.querySelectorAll('.enlarge');
+
+enlarge.forEach((el)=>{
+    observer.observe(el)
+})
