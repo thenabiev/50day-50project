43a480593cc38d8c0e1a6f486726277d3b22c348
thenabiev <sahinnabee@gmail.com>
2023-04-18 15:31:28 +0400

    day-25

--- 25day-intersection-observer/script.js
+++ 25day-intersection-observer/script.js
@@ -3,9 +3,12 @@ const cards=document.querySelectorAll(".card")
 const observer=new IntersectionObserver(entries=>{
     entries.forEach(entry=>{
         entry.target.classList.toggle ('show', entry.isIntersecting)
+       // if(entry.isIntersecting) observer.unobserve(entry.target)  // Əgər element bir dəfə observe olunubsa bir daha təkrar olmur
     })
 },{
-    threshold:0.5
+    //  threshold:0.5, // Obyektin nə qədər hissəsi ekrandan çıxırsa obyekt itir
+    rootMargin:"-100px" // Ekranın gözləm sahəsin üst və altdan 100px azaldır
+
 })
 
 cards.forEach(card=>{
